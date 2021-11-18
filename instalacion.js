eb814675cc5b5499d4bacb94c159e26eed8723ef
julianespa <julianespa12@gmail.com>
2021-11-18 11:56:44 -0300

    creo app de react

---
+++ instalacion.js
@@ -0,0 +1,4 @@
+// 1- recomienda doc de react -- en terminal (npx create-react-app nombre-app)
+// 2- descargar la libreria create react app -- (npm install -g create-react-app)(create-react-app nombre-app)
+// 3- (npm create vite nombre-app)
+// (cd nombre-app) (npm install) (npm run dev)
