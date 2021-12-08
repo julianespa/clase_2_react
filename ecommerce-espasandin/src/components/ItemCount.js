525570d5cfe09d397324d529453ddb7729b38d4c
julianespa <julianespa12@gmail.com>
2021-12-08 00:44:37 -0300

    contador final

--- ecommerce-espasandin/src/components/ItemCount.js
+++ ecommerce-espasandin/src/components/ItemCount.js
@@ -1,32 +1,36 @@
 import React, { useState} from 'react'
 
 const ItemCount = ({stock, inicial}) => {
-    let [count, setCount] = useState(inicial);
+    let [count, setCount] = useState(parseInt(inicial));
     
 
     const add = () => {
         if (count < stock) {
             setCount(count+1)
-            console.log(count)
+        }else{
+            alert(`Maximo de stock alcanzado`)
         }
     }
 
     const substract = () => {
         if (count > inicial) {
             setCount(count-1)
-            console.log(count)
+        }
+        else{
+            alert(`La cantidad no puede ser menor a ${inicial}`)
         }
     }
 
     const agregar = () => {
         console.log (`agregaste ${count} items`)
     }
+
     return (
-        <div>
+        <div className="contador">
             <p>{count}</p>
-            <button onClick={add}>+</button>
-            <button onClick={substract}>-</button>
-            <button onClick={agregar}>Agregar</button>
+            <button className="suma" onClick={add}>+</button>
+            <button className="suma" onClick={substract}>-</button>
+            <button className="agregar" onClick={agregar}>Agregar</button>
         </div>
     )
 }
